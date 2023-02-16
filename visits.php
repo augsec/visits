d84fa0bdf0efbc56da0a9b539e9152be47c67a31
Aug <frank06fk@gmail.com>
2023-02-16 09:30:16 -0300

    Introdução do bootstrap

--- visits.php
+++ visits.php
@@ -17,6 +17,8 @@ $count = $pdo->query("SELECT count(*) FROM visitors")->fetchColumn();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visitors</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 <body>
     <h1>Visitors</h1>
@@ -27,5 +29,9 @@ $count = $pdo->query("SELECT count(*) FROM visitors")->fetchColumn();
     <input onclick="location.href='insert_visits.php';" type="submit" value="Register Visit" />
         
         
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </body>
 </html>
